2e87a0598dc682c916b0c9cfc3d0508f802f36f2
Rider Adkins <rideradkins@gmail.com>
2016-11-15 18:25:50 -0500

    Removed token model and added documentation to SignedData

---
+++ PitchPointsWeb/Models/AuthenticationModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PitchPointsWeb.Models
+{
+
+    public class SignedData
+    {
+
+        /// <summary>
+        /// The plain text data that was signed
+        /// </summary>
+        public string Data { get; set; }
+
+        /// <summary>
+        /// A hexadecimal representation received from a ECDSA-secp256r1 signature
+        /// </summary>
+        public string Signature { get; set; }
+
+        public SignedData()
+        {
+
+        }
+
+    }
+}
