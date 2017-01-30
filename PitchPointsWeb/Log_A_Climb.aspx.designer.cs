eef9af354f1cce847bda32e9344ca26f1dd554c3
Sam Bruns <sam_bruns@me.com>
2017-01-30 12:13:54 -0500

    log a climb change

--- PitchPointsWeb/Log_A_Climb.aspx.designer.cs
+++ PitchPointsWeb/Log_A_Climb.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace PitchPointsWeb {
         protected global::System.Web.UI.WebControls.TextBox RouteClimbed;
         
         /// <summary>
+        /// FallsList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList FallsList;
+        
+        /// <summary>
         /// PlusFiveFalls control.
         /// </summary>
         /// <remarks>
