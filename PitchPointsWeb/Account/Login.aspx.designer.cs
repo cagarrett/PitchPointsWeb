663bfbe0b8e2a70b9fdabccb87c86ffb69753ec8
Cody <cagarrett@bsu.edu>
2017-01-31 16:44:19 -0500

    Fixed the Login page layout.

--- PitchPointsWeb/Account/Login.aspx.designer.cs
+++ PitchPointsWeb/Account/Login.aspx.designer.cs
@@ -65,14 +65,5 @@ namespace PitchPointsWeb.Account {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink RegisterHyperLink;
-        
-        /// <summary>
-        /// OpenAuthLogin control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::PitchPointsWeb.Account.OpenAuthProviders OpenAuthLogin;
     }
 }
