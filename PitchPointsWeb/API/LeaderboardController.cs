6c648da261d2464fd0785870c15ae86002ad33a1
Rider Adkins <rideradkins@gmail.com>
2017-02-27 16:01:35 -0500

    Added LeaderboardController with associated models

---
+++ PitchPointsWeb/API/LeaderboardController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PitchPointsWeb.Models;
+using static PitchPointsWeb.API.APICommon;
+using System.Data.SqlClient;
+
+namespace PitchPointsWeb.API
+{
+
+    public class LeaderboardController : ApiController
+    {
+
+        [HttpPost]
+        public HttpResponseMessage GetLeaderboard([FromBody] LeaderboardRequest request)
+        {
+            return CreateJsonResponse(InternalGetLeaderboard(request));
+        }
+
+
+        internal Leaderboard InternalGetLeaderboard(LeaderboardRequest request)
+        {
+            var connection = GetConnection();
+            connection.Open();
+            Leaderboard leaderboard = null;
+            using (var command = new SqlCommand("GetCategoryLeaderboard", connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@catId", request.CategoryId);
+                command.Parameters.AddWithValue("@compId", request.CompetitionId);
+                var reader = command.ExecuteReader();
+                leaderboard = ReadLeaderboard(reader);
+                reader.Close();
+            }
+            connection.Close();
+            return leaderboard;
+        }
+
+        private Leaderboard ReadLeaderboard(SqlDataReader reader)
+        {
+            var leaderboard = new Leaderboard();
+            leaderboard.Entries = new List<LeaderboardEntry>();
+            while (reader.Read())
+            {
+                leaderboard.Entries.Add(ReadLeaderboardEntry(reader));
+            }
+            leaderboard.Entries = leaderboard.Entries.OrderByDescending(entry => entry.Points).ToList();
+            return leaderboard;
+        }
+
+        private LeaderboardEntry ReadLeaderboardEntry(SqlDataReader reader)
+        {
+            var entry = new LeaderboardEntry();
+            entry.CompetitionId = readObject(reader, "CompetitionId", 0);
+            entry.CategoryId = readObject(reader, "CategoryId", 0);
+            entry.UserId = readObject(reader, "UserId", 0);
+            entry.Gender = readObject(reader, "Gender", false);
+            entry.FirstName = readObject(reader, "FirstName", "");
+            entry.LastName = readObject(reader, "LastName", "");
+            entry.Points = readObject(reader, "Points", 0);
+            entry.Falls = readObject(reader, "Falls", 0);
+            return entry;
+        }
+
+    }
+
+}
---
+++ PitchPointsWeb/Models/LeaderboardModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PitchPointsWeb.Models
+{
+    public class LeaderboardEntry
+    {
+
+        public int CompetitionId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int UserId { get; set; }
+
+        public bool Gender { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Points { get; set; }
+
+        public int Falls { get; set; }
+        
+    }
+
+    public class Leaderboard
+    {
+
+        public List<LeaderboardEntry> Entries { get; set; }
+
+    }
+
+    public class LeaderboardRequest
+    {
+
+        public int CompetitionId { get; set; }
+
+        public int CategoryId { get; set; }
+
+    }
+
+}
