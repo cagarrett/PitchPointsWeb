5e766a7f1702e15d59c705de0be5e883b5f702b9
Rider Adkins <rideradkins@gmail.com>
2017-03-15 23:41:23 -0400

    Created PrivateRequestModel to replace SignedData

---
+++ PitchPointsWeb/Models/API/PrivateRequestModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using PitchPointsWeb.API;
+
+namespace PitchPointsWeb.Models.API
+{
+    public class PrivateRequestModel
+    {
+
+        public string Token { get; set; }
+
+        public async Task<bool> IsValid()
+        {
+            var controller = new MasterController();
+            var status = await controller.VerifyToken(Token);
+            return status;
+        }
+
+    }
+}
