44bb6db702619999866eeb6118089090fb1eae61
Rider Adkins <rideradkins@gmail.com>
2017-03-15 23:58:34 -0400

    More changes to TokenModel

--- PitchPointsWeb/Models/API/TokenModel.cs
+++ PitchPointsWeb/Models/API/TokenModel.cs
@@ -1,19 +1,41 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using PitchPointsWeb.API;
 
 namespace PitchPointsWeb.Models.API
 {
-    public class PrivateRequestModel
+    public class TokenModel
     {
 
         public string Token { get; set; }
 
-        public async Task<bool> IsValid()
+        public async Task<TokenContentModel> ToContent()
         {
             var controller = new MasterController();
-            var status = await controller.VerifyToken(Token);
-            return status;
+            var model = await controller.ConvertTokenToModel(Token);
+            return model;
         }
 
     }
+
+    public class TokenContentModel
+    {
+        
+        public string Email { get; set; }
+
+        public DateTime ExpDateTime { get; set; }
+
+        public TokenContentModel()
+        {
+            Email = "";
+            ExpDateTime = DateTime.MinValue;
+        }
+
+        public bool IsValid()
+        {
+            return (ExpDateTime - DateTime.Now).TotalHours > 1.0;
+        }
+
+    }
+
 }
