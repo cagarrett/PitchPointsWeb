98b6f2bc5a011f6c3bf2072a5aa74a50cecd12c7
Rider Adkins <rideradkins@gmail.com>
2017-03-25 11:50:39 -0400

    Created actual definitions for Scorecards

--- PitchPointsWeb/Models/API/Response/LeaderboardControllerResponses.cs
+++ PitchPointsWeb/Models/API/Response/LeaderboardControllerResponses.cs
@@ -1,4 +1,7 @@
-﻿namespace PitchPointsWeb.Models.API.Response
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PitchPointsWeb.Models.API.Response
 {
     public class LeaderboardResponse : TokenApiResponse
     {
@@ -7,11 +10,34 @@
 
     }
 
-    public class UserSnapshotsResponse : TokenApiResponse
+    public class ScorecardResponse : TokenApiResponse
+    {
+        
+        public List<ScorecardEntry> Entries { get; set; }
+
+        public int TotalPoints
+        {
+            get
+            {
+                return Entries.Sum(e => e.Points);
+            }
+        }
+        
+    }
+
+    public class ScorecardEntry
     {
 
+        public int RouteID { get; set; }
+
+        public string WitnessName { get; set; }
+
+        public int WitnessID { get; set; }
+
+        public int Falls { get; set; }
+
         public int Points { get; set; }
-        
+
     }
 
 }
