581d26dc1e2d89501e71408f3ec42a9fb46e4183
Rider Adkins <rideradkins@gmail.com>
2017-04-11 16:34:08 -0400

    Added login / logout support to Site.master

--- PitchPointsWeb/Site.Master.designer.cs
+++ PitchPointsWeb/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace PitchPointsWeb {
     public partial class SiteMaster {
         
         /// <summary>
+        /// logoutLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor logoutLink;
+        
+        /// <summary>
         /// user_email control.
         /// </summary>
         /// <remarks>
